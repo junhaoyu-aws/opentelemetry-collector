a3f617a4744a78cafbe7dd1f4604a796178bab93
Junhao Yu <yujunhao@amazon.com>
2022-08-08 17:16:53 -0700

    goporto

--- confmap/provider/httpprovider/provider.go
+++ confmap/provider/httpprovider/provider.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package httpprovider
+package httpprovider // import "go.opentelemetry.io/collector/confmap/provider/httpprovider"
 
 import (
 	"context"
